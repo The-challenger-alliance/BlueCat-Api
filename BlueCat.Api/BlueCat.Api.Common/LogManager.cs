d4f0c523bd475a67f8748b5d30ec7f6a92ec88ca
XULIANGJIE-PC\亮杰 <xuliangjie19870208@163.com>
2017-05-25 21:33:26 +0800

    Add test code.

---
+++ BlueCat.Api/BlueCat.Api.Common/LogManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueCat.Api.Common
+{
+    public class LogManager
+    {
+    }
+
+    public enum LogLevel
+    {
+        Info,
+        Warn,
+        Error
+    }
+}
---
+++ BlueCat.Api/BlueCat.Service.Test/StudentServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BlueCat.Api.Repository.Interface;
+using BlueCat.Api.Service.Impl;
+using BlueCat.Api.Repository.Impl;
+using BlueCat.Contract;
+
+namespace BlueCat.Service.Test
+{
+    [TestClass]
+    public class StudentServiceTest
+    {
+        [TestMethod]
+        public void GetStudentsTest()
+        {
+            IStudentRepository studentRepository = new StudentRepository();
+            StudentService studentService = new StudentService(studentRepository);
+            GetStudentsRequest resquest = new GetStudentsRequest();
+            resquest.Name = "test";
+            GetStudentsResponse response = studentService.GetStudents(resquest);
+
+            if (response.ResponseResult)
+            {
+                Assert.IsTrue(response.Students.Count > 0);
+            }
+            else
+            {
+                Assert.IsFalse(true);
+            }
+        }
+    }
+}
