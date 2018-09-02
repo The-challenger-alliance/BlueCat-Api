74aace68782aa78aca1dffcc8c89e39546453061
xuliangjie <xuliangjie@shanlinjinrong.com>
2018-09-03 00:05:42 +0800

    栈代码新增

---
+++ BlueCat.Api/BlueCat.Api.Common/Log/MonitorData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueCat.Api.Common.Log
+{
+    public class MonitorData
+    {
+
+    }
+
+    /// <summary>
+    /// 侦测信息
+    /// </summary>
+    public class MonitorItem
+    { 
+    
+    }
+}
